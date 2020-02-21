9ea891495c62a349ead626a3036c58ccc66524db
Tynan Burke <tynan.burke@liveramp.com>
2020-02-21 16:25:56 -0500

    first commit!

---
+++ retrain.py
@@ -0,0 +1,28 @@
+import gpt_2_simple as gpt2
+import os
+import requests
+
+model_names = ["124M", "355M", "774M", "1558M"]
+for model_name in model_names:
+	if not os.path.isdir(os.path.join("models", model_name)):
+		print(f"Downloading {model_name} model...")
+		gpt2.download_gpt2(model_name=model_name)   # model is saved into current directory under /models/124M/
+
+
+# file_name = "shakespeare.txt"
+# if not os.path.isfile(file_name):
+# 	url = "https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt"
+# 	data = requests.get(url)
+
+# 	with open(file_name, 'w') as f:
+# 		f.write(data.text)
+
+
+sess = gpt2.start_tf_sess()
+gpt2.finetune(sess,
+              "frontline_transcripts_gpt2.txt",
+              model_name="124M",
+              steps=100,
+							save_every=15)   # steps is max number of training steps
+
+gpt2.generate(sess)
